f5e19d058a4f09327a24ec2bf2c349725ff91a3b
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-03-17 19:59:02 -0400

    Done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,32 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int numOfButtonPresses;
+        int upButton = 0;
+        int downButton= 0;
+
+        for (int i = startIndex; i < playList.length ; i++) { //checks for number of up button clicks
+            if (selection.equals(playList[i])){
+                upButton= i;
+            }
+        }
+        for (int j= startIndex; j > 0; j--){ //checks for number clicks of down button
+            if (selection.equals(playList[j])){
+                downButton= j;
+            }
+        }
+
+        if (downButton == 0) { //to look through the whole playlist
+            downButton = startIndex + playList.length - upButton;
+        }
+        if (upButton== 0){
+            upButton= playList.length - startIndex + downButton;
+        }
+        if (downButton > upButton){ //to figure out the smallest number of button presses
+            numOfButtonPresses = upButton;
+        }else {
+            numOfButtonPresses= downButton;
+        }
+        return numOfButtonPresses;
     }
 }
